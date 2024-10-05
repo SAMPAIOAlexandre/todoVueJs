01d3491a2eadc0f4e91bda41bdd27ad1b81c3fee
Alexandre SAMPAIO <alexandre.sampaio137@gmail.com>
2024-10-05 15:17:58 +0200

    feat: mise en place ajout tâche

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <script setup>
 import { ref } from "vue";
-
 const todos = ref([]);
+const todo = ref("");
+const addTodo = () => {
+	todos.value.push({
+		title: todo.value,
+		completed: false,
+		data: Date.now(),
+	});
+};
 </script>
 
 <template>
   <div>
     <h1>Hello bienvenue sur la todo list</h1>
-
+    <form @submit.prevent="addTodo">
+      <fieldset role="group">
     <input type="text"
-    placeholder="Tâche à ajouter">
+    placeholder="Tâche à ajouter"
+    v-model="todo">
     <button>Ajouter la tâche</button>
-    <div v-if="todos.length === 0"> Aucune tâche à faire :) </div>
-    <div>  
-    <ul>
-        <li>
-          tâche 1
+    </fieldset>
+    </form>
+    <div v-if="todos.length === 0"> Aucune tâche à faire </div>
+    <div v-else>
+      <ul>
+        <li v-for="todo in todos"
+        :key="todo.date">
+        {{ todo.title }}
         </li>
-        <li>
-          tâche 2
-        </li>
-      </ul>
+      </ul>  
+  
     </div>
   </div>
 
